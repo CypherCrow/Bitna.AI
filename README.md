234f54a4f7296d117e4a90648711a92395a0e37d
Jonah Morgan <jonah3254@gmail.com>
2021-10-21 01:41:39 -0700

    Edited install.sh and README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bitna.ai
 A project for the 2021 PyTorch Summer Hackathon
+
+## Setup
+
+[A suggestion: have a bitna.ai client to execute install.sh]
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# To automate activation of Python virtual environment, and installation of Python and JavaScript if not there already
+
 if []
 then 
 
 fi 
+
+source ./bitna_env/Scripts/activate
