6d967d95975807251639c4105f88e4adfa063743
Jonah Morgan <jonah3254@gmail.com>
2021-10-23 22:58:09 -0700

    Revamped Django web app and added batch scripts

---
+++ bitna_website/bitna_web_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BitnaWebAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bitna_web_app'
---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# CHECK IF PYTHON VIRTUAL ENVIRONMENT, DJANGO, AND NODE.JS ARE NOT PRESENT TO INSTALL THEM
+if []
+then 
+    
+else
+    echo "You are good to go! :)"
+fi
