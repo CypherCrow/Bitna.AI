1f40bf773c3f236698192a22eef89bee65101546
Jonah Morgan <jonah3254@gmail.com>
2021-10-25 01:26:53 -0700

    Updated bitna.cmd

--- scripts/bitna.cmd
+++ scripts/bitna.cmd
@@ -3,7 +3,9 @@
 title bitna 
 
 :: Following flags applied 
-::
 
 if [%1] == [] echo "Usage: bitna <command>"
 
+if "%1" == "install" echo "Running command install"
+
+if "%1" == "run" echo "Running bitna application"
